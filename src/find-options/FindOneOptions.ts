52ad5871add89cb1c3dc61bd31a568e067e938fd
Robinson Park <devnoff@gmail.com>
2021-10-01 07:03:01 +0900

    Update FindOneOptions.ts

--- src/find-options/FindOneOptions.ts
+++ src/find-options/FindOneOptions.ts
@@ -2,6 +2,7 @@ import {EntityFieldsNames} from "../common/EntityFieldsNames";
 import {JoinOptions} from "./JoinOptions";
 import {ObjectLiteral} from "../common/ObjectLiteral";
 import {FindConditions} from "./FindConditions";
+import { SelectQueryBuilder } from "../query-builder/SelectQueryBuilder";
 
 /**
  * Defines a special criteria to find specific entity.
@@ -16,7 +17,7 @@ export interface FindOneOptions<Entity = any> {
     /**
      * Simple condition that should be applied to match entities.
      */
-    where?: FindConditions<Entity>[]|FindConditions<Entity>|ObjectLiteral|string;
+    where?: FindConditions<Entity>[]|FindConditions<Entity>|ObjectLiteral|string|((qb: SelectQueryBuilder<Entity>) => void);
 
     /**
      * Indicates what relations of entity should be loaded (simplified left join form).
